0d6f683d0550f31ef1c3e1de8e7704812b0997ee
RickBraz <riicardobraz@gmail.com>
2020-03-14 22:42:41 -0300

    adicionei o programa

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ola usuario esse e um tradutor para a linguagem Leet Speak");
+            Console.WriteLine("digite uma frase para ver a linguagem");
+            string mensagem = Console.ReadLine() 
+                                      .Replace("a" , "4")
+                                      .Replace("A" , "")
+                                      .Replace("E" ,"")
+                                      .Replace("e" , "")
+                                      .Replace("I" , "")
+                                      .Replace("i" , "")
+                                      .Replace("L" , "")
+                                      .Replace("l" , "")
+                                      .Replace("o" , "")
+                                      .Replace("O" , "")
+                                      .Replace("T" , "")
+                                      .Replace("t" , "")
+                                      .Replace("" , "")
+                                      .Replace("" , "");
+
+        }
+    }
+}
