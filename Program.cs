d549c9b1751b2333ca8e2d3d647b50920a14dce3
= <=>
2020-05-15 00:45:04 -0300

    terminei o programa

--- Program.cs
+++ Program.cs
@@ -10,19 +10,21 @@ namespace Leet
             Console.WriteLine("digite uma frase para ver a linguagem");
             string mensagem = Console.ReadLine() 
                                       .Replace("a" , "4")
-                                      .Replace("A" , "")
-                                      .Replace("E" ,"")
-                                      .Replace("e" , "")
-                                      .Replace("I" , "")
-                                      .Replace("i" , "")
-                                      .Replace("L" , "")
-                                      .Replace("l" , "")
-                                      .Replace("o" , "")
-                                      .Replace("O" , "")
-                                      .Replace("T" , "")
-                                      .Replace("t" , "")
-                                      .Replace("" , "")
-                                      .Replace("" , "");
+                                      .Replace("A" , "4")
+                                      .Replace("E" ,"3")
+                                      .Replace("e" , "3")
+                                      .Replace("I" , "1")
+                                      .Replace("i" , "1")
+                                      .Replace("L" , "1")
+                                      .Replace("l" , "1")
+                                      .Replace("o" , "0")
+                                      .Replace("O" , "0")
+                                      .Replace("T" , "7")
+                                      .Replace("t" , "7")
+                                      .Replace("s" , "5")
+                                      .Replace("S" , "5");
+            
+            Console.WriteLine(mensagem);
 
         }
     }
